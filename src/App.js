ba55c9edf73aa3dcc6aa2f967f9284d0010d5798
Winnie Xiao <winniex@stanford.edu>
2020-08-18 21:21:23 -0400

    Spotify Web API wrapper incorporated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from "react";
+import { clientId, redirectUri } from "./config";
+
 // import axios from "axios";
-
 import { Button } from "antd";
-
 import "./App.css";
+
+// Lightweight wrapper for Spotify Web API, see
+// https://github.com/JMPerez/spotify-web-api-js/blob/master/src/spotify-web-api.js
+import SpotifyWebApi from "spotify-web-api-js";
+const spotifyApi = new SpotifyWebApi();
 
 // The ReactDOM is rendering <App/> in index.js
 
 function App() {
-  const [login, setLogin] = useState(false);
   const [token, setToken] = useState("");
-  const [auth, setAuth] = useState(false);
-
-  // const client_id = "29e77da78fb34f17bbc5f5bab141a50d"; // Your client id
-  // const client_secret = "164c8c38eba044e49c570a06e8372192"; // Your secret
-  // const redirect_uri = "http://localhost:3000/callback"; // Your redirect uri
 
   useEffect(() => {
-    if (token === "" && window.location.hash != "") {
+    if (token === "" && window.location.hash !== "") {
       console.log("Fetching the token now that we are logged in");
       const hash = window.location.hash
         .substring(1)
@@ -29,20 +28,48 @@ function App() {
           }
           return initial;
         }, {});
+      spotifyApi.setAccessToken(hash.access_token);
       setToken(hash.access_token);
     }
-  }, [token, login]);
+  }, [token]);
 
   function requestAccess() {
     console.log("Authorize request received.");
     window.location.href =
-      "https://accounts.spotify.com/authorize?client_id=29e77da78fb34f17bbc5f5bab141a50d&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback";
+      "https://accounts.spotify.com/authorize?client_id=" +
+      clientId +
+      "&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=" +
+      redirectUri;
+  }
+
+  // function getPlaylists() {
+  //   const playlists = axios
+  //     .get("https://api.spotify.com/v1/me/playlists", {
+  //       headers: { Authorization: "Bearer " + token },
+  //     })
+  //     .then((response) => {
+  //       console.log(response);
+  //     })
+  //     .catch((error) => {
+  //       console.log("Token: ", token);
+  //       console.log(error);
+  //     });
+  //   return playlists;
+  // }
+
+  function getPlaylists() {
+    spotifyApi
+      .getUserPlaylists()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
   }
 
   return (
     <div className="App">
       {window.location.hash || token ? (
-        <Button>Authorized with the Access Token</Button>
+        <Button onClick={getPlaylists}>Authorized with the Access Token</Button>
       ) : (
         <Button type="primary" onClick={requestAccess}>
           Grant Spotify Permission
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const clientId = "29e77da78fb34f17bbc5f5bab141a50d"; // Your client id
+export const clientSecret = "164c8c38eba044e49c570a06e8372192"; // Your secret
+export const redirectUri = "http://localhost:3000/callback"; // Your redirect uri
