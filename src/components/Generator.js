55f5ba6948ce72d064bcaef46e5bef05a04a678c
Winnie Xiao <winniex@stanford.edu>
2020-08-25 23:27:42 -0400

    Started algorithm

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -4,11 +4,14 @@ import { Spin, Typography, Row } from "antd";
 const { Title } = Typography;
 
 function Generator({ tracks, tags }) {
+  function generateArt() {}
   return (
     <div>
       <Row justify="center" align="bottom" style={{ minHeight: "45vh" }}>
         <Typography>
           <Title level={2}>Generating Cover Art...</Title>
+          {console.log("tracks: ", tracks)}
+          {console.log("tags: ", tags)}
         </Typography>
       </Row>
       <Row justify="center">
