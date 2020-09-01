3f07b5d03842186a30fc4aee5ec85e35371b2775
Winnie Xiao <winniex@stanford.edu>
2020-09-01 01:04:51 -0400

    Added Artsy API token auth and search and string concat

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 var express = require("express"); // Express web server framework
-var request = require("request"); // "Request" library
+var request = require("superagent");
 var cors = require("cors");
+var axios = require("axios");
 var querystring = require("querystring");
 var cookieParser = require("cookie-parser");
 
-var client_id = "29e77da78fb34f17bbc5f5bab141a50d"; // Your client id
-var client_secret = "164c8c38eba044e49c570a06e8372192"; // Your secret
-var redirect_uri = "http://localhost:3000/callback"; // Your redirect uri
+var clientID = "503e2e0e5362808c901a",
+  clientSecret = "a88889d6d635b5174682d08538bca175",
+  apiUrl = "https://api.artsy.net/api/tokens/xapp_token",
+  xappToken;
+
+// r = request
+//   .post(apiUrl)
+//   .send({ client_id: clientID, client_secret: clientSecret })
+//   .end(function (res) {
+//     xappToken = res.body.token;
+//   });
+
+// j = json.load(r.text);
+// token = j["token"];
 
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function (length) {
-  var text = "";
-  var possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-
-var stateKey = "spotify_auth_state";
 
 var app = express();
 
@@ -33,121 +33,18 @@ app
   .use(cors())
   .use(cookieParser());
 
-app.get("/login", function (req, res) {
-  console.log("In log in");
-  var state = generateRandomString(16);
-  res.cookie(stateKey, state);
-
-  // your application requests authorization
-  var scope = "user-read-private user-read-email";
-  res.redirect(
-    "https://accounts.spotify.com/authorize?" +
-      querystring.stringify({
-        response_type: "code",
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
-        state: state,
-      })
-  );
-});
-app.get("/callback", function (req, res) {
-  // your application requests refresh and access tokens
-  // after checking the state parameter
-  console.log("In redirected URI");
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
-
-  if (state === null || state !== storedState) {
-    res.redirect(
-      "/#" +
-        querystring.stringify({
-          error: "state_mismatch",
-        })
-    );
-  } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: "https://accounts.spotify.com/api/token",
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: "authorization_code",
-      },
-      headers: {
-        Authorization:
-          "Basic " +
-          new Buffer(client_id + ":" + client_secret).toString("base64"),
-      },
-      json: true,
-    };
-
-    request.post(authOptions, function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        var access_token = body.access_token,
-          refresh_token = body.refresh_token;
-
-        var options = {
-          url: "https://api.spotify.com/v1/me",
-          headers: { Authorization: "Bearer " + access_token },
-          json: true,
-        };
-
-        // use the access token to access the Spotify Web API
-        request.get(options, function (error, response, body) {
-          console.log(body);
-        });
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect(
-          "/#" +
-            querystring.stringify({
-              access_token: access_token,
-              refresh_token: refresh_token,
-            })
-        );
-      } else {
-        res.redirect(
-          "/#" +
-            querystring.stringify({
-              error: "invalid_token",
-            })
-        );
-      }
-    });
-  }
-});
-
-app.get("/refresh_token", function (req, res) {
-  // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: "https://accounts.spotify.com/api/token",
-    headers: {
-      Authorization:
-        "Basic " +
-        new Buffer(client_id + ":" + client_secret).toString("base64"),
-    },
-    form: {
-      grant_type: "refresh_token",
-      refresh_token: refresh_token,
-    },
-    json: true,
-  };
-
-  request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        access_token: access_token,
-      });
-    }
-  });
-});
-
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("/generate", function (req, res) {
+  request
+    .post(apiUrl)
+    .send({ client_id: clientID, client_secret: clientSecret })
+    .then((response) => {
+      xappToken = response.body.token;
+      res.send(xappToken);
+    });
 });
 
 app.listen(process.env.PORT || 8080);
--- src/components/Generator.js
+++ src/components/Generator.js
@@ -1,20 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { Spin, Typography, Row } from "antd";
+import axios from "axios";
+import request from "superagent";
 
 const { Title } = Typography;
+const apiUrl = "https://api.artsy.net/api/search?type=";
 
 function Generator({ tracks, tags }) {
+  // let queryString = "";
+
   function generateArt() {
-    // checkout runaway ML
-    // just broke up yesterday lol
+    request
+      .get("/generate")
+      .then((response) => {
+        let query = collectNamesAndTags();
+        request
+          .get(apiUrl)
+          .set("X-Xapp-Token", response.text)
+          .query({ q: query })
+          .query({ type: "artwork" })
+          .then((res) =>
+            console.log(
+              "artsy's relevant artwork: ",
+              res.body._embedded.results[0]
+            )
+          )
+          .catch((err) => console.log(err));
+      })
+      .catch((error) => console.log(error));
   }
+
+  // Returns the songNames array of the first 20 songs
+  function collectNamesAndTags() {
+    let songs = tracks.items;
+    let limit = 20;
+    if (songs.length < 20) {
+      limit = songs.length;
+    }
+    for (let i = 0; i < limit; i++) {
+      let currSongName = songs[i].track.name;
+      currSongName.slice(0, currSongName.indexOf("-")); // strips string after '-' to get just name
+      let n = currSongName.indexOf("(");
+      currSongName = currSongName.substring(
+        0,
+        n != -1 ? n : currSongName.length
+      ); // strips string after '-' to get just name
+      tags.push(currSongName);
+    }
+    return tags.join(" ");
+  }
+
   return (
     <div>
       <Row justify="center" align="bottom" style={{ minHeight: "45vh" }}>
         <Typography>
           <Title level={2}>Generating Cover Art...</Title>
-          {console.log("tracks: ", tracks)}
-          {console.log("tags: ", tags)}
+          {collectNamesAndTags()}
+          {generateArt()}
         </Typography>
       </Row>
       <Row justify="center">
