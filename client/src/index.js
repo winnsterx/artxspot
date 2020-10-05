69ce6c48f2cdf97eaa01696fc2530df250e603ac
Winnie Xiao <winniex@stanford.edu>
2020-10-05 11:00:37 +0200

    Fixed up some old shit, removed config.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+// index.js renders the react components we use
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
--- server/src/server.js
+++ server/src/server.js
@@ -2,22 +2,23 @@ var express = require("express"); // Express web server framework
 var request = require("superagent");
 var os = require("os");
 
-var clientID = "503e2e0e5362808c901a",
-  clientSecret = "a88889d6d635b5174682d08538bca175",
-  apiUrl = "https://api.artsy.net/api/tokens/xapp_token",
-  xappToken;
+// var clientID = "503e2e0e5362808c901a",
+//   clientSecret = "a88889d6d635b5174682d08538bca175",
+//   apiUrl = "https://api.artsy.net/api/tokens/xapp_token",
+//   xappToken;
 
 var app = express();
 
-app.get("/generate", function (req, res) {
-  console.log("Calling generate, trying to make a request call in server.js");
-  request
-    .post(apiUrl)
-    .send({ client_id: clientID, client_secret: clientSecret })
-    .end(function (response) {
-      xappToken = response.body.token;
-    });
-});
+// Moved to the frontend.
+// app.get("/generate", function (req, res) {
+//   console.log("Calling generate, trying to make a request call in server.js");
+//   request
+//     .post(apiUrl)
+//     .send({ client_id: clientID, client_secret: clientSecret })
+//     .end(function (response) {
+//       xappToken = response.body.token;
+//     });
+// });
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
